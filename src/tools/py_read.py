cc09b94ba37cc3af973b45910827a3a8772e9be0
Nikhil Bhangale <bhangale7057@gmail.com>
2026-06-14 22:58:12 +0530

    [10] Added doc string to py_read file

--- src/tools/py_read.py
+++ src/tools/py_read.py
@@ -2,7 +2,14 @@ from langchain.tools import tool
 
 @tool
 def py_read(file_name:str) -> str:
-    """py_red function read .py python files and return their code"""
+    """py_red function read .py python files and return their code
+    
+    Args:
+    file_name:str
+
+    return:
+    str
+    """
     with open(file_name,"r") as file:
         data = file.read()
     return data
