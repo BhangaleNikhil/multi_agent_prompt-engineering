d217780105311ee9a0a44fe342157a29160526f8
Nikhil Bhangale <bhangale7057@gmail.com>
2026-06-16 23:33:30 +0530

    [18] Added fields to reports state to track token usage

--- src/states/report_doc_state.py
+++ src/states/report_doc_state.py
@@ -5,3 +5,7 @@ class ReportDocState(TypedDict):
     docs:List[str]
     processed_docs:Optional[List[str]]
     docs_with_issues:Optional[List[str]]
+    input_tokens_tool:int
+    output_tokens_tool:int
+    input_tokens_agent:int
+    output_tokens_agent:int
